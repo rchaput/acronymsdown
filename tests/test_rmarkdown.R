d21d29602fce3e87111a6ef372196e0968c55646
rchaput <rchaput.pro@gmail.com>
2022-01-04 22:26:44 +0100

    Refactored tests for better readability.
    Moved all the calls to 'testthat::test_that' in a separate file ('test_rmarkdown.R') and replaced them to 'testthat::test_file' in the main
    'testthat.R' script.
    In practice, this means that testthat creates a nice report that is easier to read.
    Also, configured 'rmarkdown' to be quiet, which reduces the clutter in the output. (We do not care what files are created anyway, since they are temporary and destroyed after the test ends)

---
+++ tests/test_rmarkdown.R
@@ -0,0 +1,47 @@
+library(testthat)
+library(rmarkdown)
+library(acronymsdown)
+
+# Each sub-directory in this `tests` directory contains an example
+# of a Rmd document (`input.Rmd`), and an expected output (`expected.md`).
+# For each of these directories, we test in this file that the rendered
+# output, using `rmarkdown::render` and our `acronymsdown` package,
+# actually corresponds to the expected file.
+
+# Helper function which tests that a single directory works correctly.
+test_dir <- function(dirname) {
+
+  out_path <- tempfile(dirname, fileext = ".md")
+  input_path <- file.path(getwd(), dirname, "input.Rmd")
+  expected_path <- file.path(getwd(), dirname, "expected.md")
+
+  rmarkdown::render(input_path,
+                    output_file = out_path,
+                    quiet = TRUE,
+  )
+
+  lines_expected <- readLines(expected_path)
+  lines_out <- readLines(out_path)
+
+  testthat::expect_identical(lines_out, lines_expected)
+}
+
+folders <- c(
+  "01-simple",
+  "02-custom-title",
+  "03-no-title",
+  "04-replace-duplicates",
+  "05-keep-duplicates",
+  "06-external-yaml",
+  "07-multiple-external-yaml",
+  "08-style-short-long",
+  "09-style-footnote"
+)
+
+for (folder in folders) {
+  # The first argument is the test's name (which is set to the folder's
+  # name for readability).
+  testthat::test_that(folder, {
+    test_dir(folder)
+  })
+}
